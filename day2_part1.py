bef0ac07343fc45d2d465b5fdd3201ad4682a0f6
Kate Casey <katelcasey@gmail.com>
2022-12-05 20:10:09 +1000

    Rewrite to construct a rulebook and match to that

--- day2_part1.py
+++ day2_part1.py
@@ -7,17 +7,29 @@ draw = 3
 lose = 0
 totalScore = 0
 
+# list of lists to hold the rules - their move, my move, my outcome (added later), my score (added later)
+ruleBook = []
+ruleBook.append(['A', 'X'])
+ruleBook.append(['A', 'Y'])
+ruleBook.append(['A', 'Z'])
+ruleBook.append(['B', 'X'])
+ruleBook.append(['B', 'Y'])
+ruleBook.append(['B', 'Z'])
+ruleBook.append(['C', 'X'])
+ruleBook.append(['C', 'Y'])
+ruleBook.append(['C', 'Z'])
+
 
 def getOutcome(theirMove, myMove):
     outcome = lose
-        #their scissors loses to my rock
-    if theirMove == 'C' and myMove == 'X':
-        outcome = win
         #their rock loses to my paper
     if theirMove == 'A' and myMove == 'Y':
         outcome = win
         #their paper loses to my scissors
     if theirMove == 'B' and myMove == 'Z':
+        outcome = win
+        #their scissors loses to my rock
+    if theirMove == 'C' and myMove == 'X':
         outcome = win
         #rock and rock draws
     if theirMove == 'A' and myMove == 'X':
@@ -31,25 +43,41 @@ def getOutcome(theirMove, myMove):
     return outcome
 
 
+def getScore(outcome, myMove):
+    if myMove == 'X': 
+        myMoveNum = rock
+    if myMove == 'Y': 
+        myMoveNum = paper
+    if myMove == 'Z': 
+        myMoveNum = scissors    
+    
+    return outcome + myMoveNum 
+
+
+#Calculate the outcomes and scores and populate the rulebook
+for move in ruleBook:
+    outcome = getOutcome(move[0], move[1])
+    move.append(outcome)
+    move.append(getScore(outcome, move[1]))
+
+
+#read in the file values and determine the result
 with open('day2_input.txt', 'r') as file:
-    move = 1
+    play = 1
     score = 0
     for line in file:
-        key = 'move' + str(move)
+        key = 'play' + str(play)
         cleanLine = line.replace('\n', '')
         theirMove, myMove = cleanLine.split(' ')
         instr[key] = []
         instr[key].append(theirMove)        
         instr[key].append(myMove)
-        if myMove == 'X': 
-            instr[key].append(rock)
-        if myMove == 'Y': 
-            instr[key].append(paper)
-        if myMove == 'Z': 
-            instr[key].append(scissors)
-        score = instr[key][2] + getOutcome(instr[key][0], instr[key][1]) 
-        instr[key].append(score)
-        move += 1
+        #iterate through rule book to find the matching outcome and score, populate the record
+        for rule in ruleBook:
+            if rule[0] == theirMove and rule[1] == myMove:
+                instr[key].append(rule[2]) #append the matching outcome
+                instr[key].append(rule[3]) #append the matching score
+        play += 1
 
 
 for game in instr.keys():    
